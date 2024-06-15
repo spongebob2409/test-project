75fae8f68d48637b338fb7e1809552180a03bcab
Abrar Mahir Rohan <154866494+spongebob2409@users.noreply.github.com>
2024-06-16 01:09:21 +0600

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const UserModel = require('./models/User')
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect('mongodb://127.0.0.1:27017/teaching');
+
+
+app.post('./login', (req, res) =>{
+    const {email, password} = req.body;
+    UserModel.findOne({email: email})
+    .then(user =>{
+        if(user) {
+            if(user.password == password){
+                res.json("success")
+            } else {
+                res.json("the password is incorrect")
+            }
+
+        }
+    })
+})
+
+app.post('./register', (req, res) => {
+    UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+
+})
+
+app.listen(3001, () => {
+    console.log("server is running")
+})
